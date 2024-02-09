069eb6085b9af11a36393574daf6c12f97efbafd
Erika Gonzalez <erikagonzz01@gmail.com>
2024-02-09 15:32:19 -0500

    fixed the favorites controller to add and ramove items favorited

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,24 +1,38 @@
 class FavoritesController < ApplicationController
-  
+  before_action :authenticate_user
+
   def index
-    @favorites = current_user.favorites
-    render json: @favorites
+    @favorites = current_user.favorites.includes(:item)
+    render json: @favorites.map { |favorite| favorite.as_json.merge(item: favorite.item.as_json(only: [:id, :name, :description, :category, :image_url]))}
   end
 
   def create
-    favorite = current_user.favorites.build(favorite_params)
-    if favorite.save
-      render json: favorite, status: :created
+    @favorite = current_user.favorites.build(favorite_params)
+    if @favorite.save
+      render json: @favorite, status: :created
     else
-      render json: favorite.errors, status: :unprocessable_entity
+      render json: @favorite.errors, status: :unprocessable_entity
     end
   end
+  
+  # def create
+  #   @favorite = Favorite.new(favorite_params)
+  #   if @favorite.save
+  #     render json: @favorite, status: :created
+  #   else
+  #     render json: favorite.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   def show
-    favorite = current_user.favorites.find_by(id: params[:id])
-    render json: favorite
+    @favorite = current_user.favorites.includes(:item).find_by(id: params[:id])
+    if @favorite
+      render json: @favorite, include: { item: {only: [:id, :name, :description, :category, :image_url]}}
+    else
+      render json: {error: "Favorite not found"}, status: :not_found
+    end
   end
-
+  
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id])
     if favorite
@@ -28,5 +42,17 @@ class FavoritesController < ApplicationController
       render json: { error: "Favorite not found" }, status: :not_found
     end
   end
+
+  private
+
+
+  def favorite_params
+    params.require(:favorite).permit(:item_id)
+  end
+
+
+  # def favorite_params
+  #   params.permit(:item_id, :user_id)
+  # end
   
 end
